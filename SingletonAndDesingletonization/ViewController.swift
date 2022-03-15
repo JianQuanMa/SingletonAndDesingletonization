cc65f1ba6917e690bcb522b4992556aa2cb47535
Jian Ma <jianquanma@gmail.com>
2022-03-14 22:08:26 -0400

    Initial Commit

---
+++ SingletonAndDesingletonization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingletonAndDesingletonization
+//
+//  Created by Jian Ma on 3/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
